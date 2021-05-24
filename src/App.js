fa33a09f539fbe76b57c565d4e3a37be13f078d8
sameer sheikh <sameersheikh819@gmail.com>
2021-05-24 19:51:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Name from './Components/Input/user-input';
+
+function App(){
+  return (
+    <div className="App">
+    <Name/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Input/user-input.js
@@ -0,0 +1,49 @@
+import React, { useState , useEffect} from "react";
+
+//import { ThemeContext, LocaleContext} from './components'
+
+function Name() {
+  const [Name, setName] = useState("");
+  const [Number, setNumber] = useState("");
+  //const theme = useContext(ThemeContext);
+
+  useEffect(() =>{
+      document.title= Name + '' + Number;
+  });
+const [Width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener ('resize', handleResize);
+     return () => {
+      window.removeEventListener ('resize', handleResize);}
+  })
+  function onChangeHandler(e) {
+    setName(e.target.value);
+  }
+
+  function onChangeNumber(e) {
+    setNumber(e.target.value);
+  }
+
+  return (
+      <div class="card">
+          <input
+              type="text"
+              value={Name}
+              placeholder="Sameer sheikh"
+              onChange={onChangeHandler}
+          />
+          <h1>Name={Name}</h1>
+          <input
+              type="number"
+              value={Number}
+              placeholder="123456789"
+              onChange={onChangeNumber}
+          />
+          <h1>Number={Number}</h1>
+          <h3>width={Width}</h3>
+      </div>
+  );
+}
+
+export default Name;
