09a86ea03bde42a4af8f00f65b321610f6831665
Sohel Sheikh <sohel.sheikh@thoughtworks.com>
2021-05-28 00:39:21 +0530

    fix counter init value error

--- src/Components/Input/count.js
+++ src/Components/Input/count.js
@@ -1,23 +1,22 @@
-import React, { useState} from 'react';
-
+import React, { useState } from "react";
 
 const Counter = () => {
-    const [count, setCount] = useState("0");
+  const [count, setCount] = useState(0);
 
-const onIncrement = () => {
-setCount(count+1);
-};
-const onDecrement = () => {
-setCount(count-1);
-};
-    return(
-        <div>
-            <h1>Count : {count}</h1>
-            <button
-            onClick={onDecrement}>-</button>
-            <button onClick={onIncrement}>+</button>
-        </div>
-    )
+  const onIncrement = () => {
+    setCount(count + 1);
+  };
+  const onDecrement = () => {
+    setCount(count - 1);
+  };
 
-}
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={onDecrement}>-</button>
+      <button onClick={onIncrement}>+</button>
+    </div>
+  );
+};
+
 export default Counter;
