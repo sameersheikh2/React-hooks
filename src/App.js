0f086f054658ee4d94c704a527eaa125efd0bc17
sameer sheikh <sameersheikh819@gmail.com>
2021-05-28 10:48:42 +0530

    src/App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import './App.css';
-//import Name from './Components/Input/user-input';
-import Counter from './Components/Input/count'
+import React from "react";
+import "./App.css";
+import Indx from "./Components/Routers/Route-page";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Counter from "./Components/Input/count";
+import Tutorial1 from "./Components/Routers/Tutorial1";
+import Tutorial2 from "./Components/Routers/Tutorial2";
+import Tutorial3 from "./Components/Routers/Tutorial3";
+import Tutorial4 from "./Components/Routers/Tutorial4";
 
-function App(){
+function App() {
   return (
     <div className="App">
-    <Counter/>
-    
+      <Counter />
+      <Router>
+        <Indx />
+        <Route path="/Tutorial1" component={Tutorial1} />
+        <Route path="/Tutorial2" component={Tutorial2} />
+        <Route path="/Tutorial3" component={Tutorial3} />
+        <Route path="/Tutorial4" component={Tutorial4} />
+      </Router>
     </div>
   );
 }
