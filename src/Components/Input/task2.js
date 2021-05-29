00cbedd63cd94013f09aeaf571da1969eec23de7
sameer sheikh <sameersheikh819@gmail.com>
2021-05-29 20:46:16 +0530

    add input in counter

--- src/Components/Input/task2.js
+++ src/Components/Input/task2.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <h1>Counter</h1>;
+  const [num, setNum] = useState("6");
+  return (
+    <div>
+      <h1>Counter</h1>
+      <input
+        type="text"
+        value={num}
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      />
+    </div>
+  );
 };
 export default Counter;
